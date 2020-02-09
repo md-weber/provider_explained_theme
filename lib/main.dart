778215be06f183d2b9ea85ee7b4e6deac4bf5388
myracledesign <weber.max.1988@live.de>
2020-02-09 11:29:57 +0100

    added todos so you can follow along

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider_explained_theme/screens/login_screen.dart';
 
 void main() {
+  // TODO: read from the sharedPreferences
   return runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // TODO: Use the Provider to select the sharedPreference
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light(),
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,1 +1,12 @@
-class ThemeProvider {}
+// TODO: Make this class a ChangeNotifier
+class ThemeProvider {
+  // TODO: We need a selectedTheme
+
+  // TODO: Declare a dark and a light theme.
+
+  // TODO: Constructor to select a dark or light theme.
+
+  // TODO: Implement a swapTheme method that changes the current theme
+
+  // TODO: getTheme method
+}
